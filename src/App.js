29a44b3fde4ef1bb547fdc0512bb9a280b07ff7d
Serin <bcode429@gmail.com>
2021-03-09 23:06:44 +0900

    JSX 기초 다루기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '혜화 맛집';
+  function myfunc() {
+    return 100
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        menu
+      </div>
+      <h4> { posts } </h4>
+      <h4> { myfunc() } </h4>
+      <img src={ logo } alt="logo" />
+      <div style={ {color: "blue", fontSize: "30px" }}>스타일 변경</div>
     </div>
   );
 }
